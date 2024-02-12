042feb9816645756bc01dabdc5c006870b8001fa
huzaifa <huzziaf@gmail.com>
2024-02-12 09:54:36 +0500

    dev file changed

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def functionMaster():
     print("This function was added in the master branch.")
 
+def functionDev():
+    print("This function was added in the dev branch.")
 
-if __name__ == '__main__':
+
+def functionTest():
+    print("This function was added in the test branch.")
+
+if _name_ == '_main_':
     functionMaster()
-
+    functionTest()
+    functionDev()
