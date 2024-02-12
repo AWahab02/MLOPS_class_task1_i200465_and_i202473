fa47535d73445e48a9812bcfa4315b672e0843f7
AWahab02 <awzahid02@gmail.com>
2024-02-12 09:20:32 +0500

    test branch done

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def functionMaster():
     print("This function was added in the master branch.")
 
 
+def functionTest():
+    print("This function was added in the test branch.")
+
 if __name__ == '__main__':
     functionMaster()
+    functionTest()
 
